d2b4cffc56e962fd1aeb73ccf3f2acc952862dca
Romi Strub <romistrub@gmail.com>
2011-02-19 17:13:29 -0500

    Untested prototype of components

--- lib/Glassware.rb
+++ lib/Glassware.rb
@@ -1,101 +1,139 @@
-module Atom
-  def to_atom
-    # access object properties to grab important information
-  end
-  def from_atom(atom)
-    # 
-  end
-end
+NA
+NA::Atom
+NA::CL
+NA::CL::Output
+NA::CL::Membrane
+NA::CL::Vessel
+NA::CL::Pipe
 
-module Interface
-  def decode(atom)
-    
-  end
-  def add(*atoms)
-    atoms.collect{|i| decode i}
-  end
-end
-
-# Pipe builds on ideas from MIDIator::Timer
-module Glassware
+module NA
   
-  # A pipe is completely agnostic to the content of the $queue
-  class Pipe
+  module Atom
     
-    def initialize(args, &router)
-      $queue = []
+    def to_atom
+      # access object properties to grab important information
+    end
+  
+  end
+  
+  module CL
+
+    def to(component)
+    $next_component = component
+    self
+    end
+    
+    private
+    
+    def pass(*atoms)
+    $next_component.add atoms
+    end
+    
+    def process(*atoms, &process)
+    atoms.collect{|i| (process || $process).call i}
+    end
+    
+    class Component
+    include CL
+      
+      def initialize(next_component, &process)
+      $next_component = next_component
+      $process = process
+      end
+  
+      def add(*atoms)
+      end  
+    
+    end
+    
+    class Membrane < Component
+    
+      def add(*atoms)
+      atoms && pass(process(atoms))
+      end
+    
+    end
+      
+    class Vessel < Component
+      
+      def initialize
+      super
+      $contents = []
+      $overflow = nil
+      $size = 0
+      end
+      
+      def to_ary
+      $contents
+      end
+      
+      def add(*atoms)
+      $contents.push(atoms.slice!(0, $size-$contents.size))
+      $overflow && $overflow.add(atoms)
+      end
+
+      def extract(size=1)
+      $contents.shift(size)
+      end
+      
+      def drip
+      extract
+      end
+      
+      def drain
+      extract($contents.size)
+      end
+         
+      def start_scan(period=nil, res=10)
+        stop_scan
+        $scanner = Thread.new {
+          loop {
+            t = Time.now.to_f
+            if !period || (lastRun-t > period)
+              lastRun = t
+              pass(process(drip))
+            end
+            sleep res
+          }
+        }
+      end
+      
+      def stop_scan
+        $scanner && $scanner.kill
+      end
+      
+    end
+  
+    class Pipe < Vessel
+      
+      def initialize
+      super
       $pressure = args[:pressure] || 1
       $width = args[:grouping] || 1
-      $size = args[:length] || 0
-      $overflow = args[:overflow] || nil
-      $router = router
-    end
-    
-    def add(*atoms)
-      $queue.push(atoms.slice!(0, $size-$queue.size))
-      $overflow && $overflow.add(atoms)
+      end
+      
+      def drip
+      extract($width*$pressure)
+      end
       
     end
-    
-    ## MANUAL PULL METHODS GRAB ATOMS
-    
-    # draws a specific number of atoms from the pipe, according to the pressure applied
-    def leak(pressure=$pressure)
-      $queue.shift(pressure)
-    end
-    
-    # pressure determines frequency of drips
-    def drip
-      $queue.shift($width)
-    end
-    
-    # flow
-    def flow
-      $queue.shift($pressure*$width)
-    end
-    
-    
-    
-    def drain
-    end
-    
-    # can route arbitrary atoms -- could be its own class, like "reaction"
-    def route(*atoms)
-      atoms.each {|atom|
-        $router.call(atom).add(atom)
-      }
-    end
-    
-    def join
-    end
-    
-    def open(w=1, period=nil, res=10)  # can be used as a drip or an open valve
-      close
-      $scanner = Thread.new {
-        loop {
-          t = Time.now.to_f
-          if !period || (lastRun-t > period)
-            lastRun = t
-            self.flow
-          end
-          sleep res
-        }
-      }
-    end
-    
-    def close
-      $scanner && $scanner.kill
-    end
-  
-  end
-  
-  class Vessel
-  end
-  
-  class Hopper < Vessel
-    def initialize
-      super
+
+    module Output
+    include Node
+      
+      def from_atom(*atom)
+      end
+      
+      def add(*atoms)
+      atoms && process(atoms)
+      end
       
     end
+
   end
+      
 end
+
+
+  
+  
